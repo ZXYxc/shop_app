742904a604130c00b4d7aa16ab2b8c5b4f190dc0
yyp0 <1941879233@qq.com>
2020-06-11 23:53:43 +0800

    yyp

--- src/yyp's part/js/FPage.js
+++ src/yyp's part/js/FPage.js
@@ -9,8 +9,9 @@ var showPics = new Vue({
 	},
 	methods: {
 		show() {
+			console.log("show");
 			showPics.nowPic = showPics.pics[showPics.i%4];
-			//console.log(showPics.nowPic);
+			console.log(showPics.nowPic);
 			for(var j=1; j<=4; j++){
 				if((this.i%4+1) == j){
 					document.getElementById(this.id+j).setAttribute("class","cycle1");
@@ -18,17 +19,20 @@ var showPics = new Vue({
 					document.getElementById(this.id+j).setAttribute("class","cycle");
 				}
 			}
+			console.log(this.i);
 			this.i += 1;
 		},
 		play() {
+			console.log("play");
 			setInterval(this.show, 2000);
 		},
 		getPics() {
 			axios.get("http://127.0.0.1:8000/goods/ListItem/", {params: ""}).then(function(response){
+				console.log("getPics");
 				var data = response['data']['items'];
-				console.log(data[0]);
+				//console.log(data[0]);
 				for(var i=0; i<data.length; i++){
-					showPics.pics.push(data[i]['picPath1']);
+					showPics.pics.push("http://"+data[i]['picPath1']);
 					if(showPics.pics.length == 4) break;
 				}
 				console.log(showPics.pics);
@@ -49,42 +53,46 @@ var className = new Vue({
 		theme2: "",
 		theme3: "",
 		theme4: "",
-		picPath1: "",
-		picPath2: "",
-		picPath3: "",
-		picPath4: "",
+		picPath:[
+			"./img/picError.jpg",
+			"./img/picError.jpg",
+			"./img/picError.jpg",
+			"./img/picError.jpg"
+		],
 	},
 	methods: {
 		getTheme(){
-			/*
-			axios.get("xxx").then(function(response){
-				
-					theme-href需要返回type字段
-					picPath需要获取图片路径
-				
+			axios.get("http://127.0.0.1:8000/goods/ListItem/").then(function(response){
+				if(response['data']['result']=="success"){
+					var data = response['data']['items'];
+					console.log(data);
+					for(var i=0;i<4;i++){
+						var img = document.getElementById("classPic"+i); // 主题图
+						//console.log("classPic"+i);
+						//console.log(img);
+						//console.log(i);
+						img.setAttribute("src","http://"+data[i]['picPath1']);
+						/*
+						 type = data[i]['type'];
+						 id = data[i]['itemId'];
+						 
+						*/
+					}
+					//console.log(className.picPath);
+				}
 			})
-			*/
+			
 		}
 	},
-	/*created: function(){
-		Vue.$http.get("url").then(function(successData){  
-			var themes = JSON.parse(successData);   // [{url:"xxx",path:"xxx"},...]
-			this.theme1 = themes[0]['url'];
-			this.theme2 = themes[1]['url'];
-			this.theme3 = themes[2]['url'];
-			this.theme4 = themes[3]['url'];
-			this.picPath1 = themes[0]['path'];
-			this.picPath2 = themes[1]['path'];
-			this.picPath3 = themes[2]['path'];
-			this.picPath4 = themes[3]['path'];
-		})	
-	}*/
+	created: function(){
+		this.getTheme();
+	}
 });
 
 var showItems = new Vue({
 	el: "#showItems",
 	data: {
-		url: "./lookUpItem?itemId=",
+		url: "./lookUpItem?itemId=",  // xy,这里改成你那边的路由
 		items: [],
 	},
 	methods: {
@@ -95,8 +103,13 @@ var showItems = new Vue({
 					var data=response['data']['items'];
 					for(var i=0; i<data.length; i++){
 						showItems.items.push({
-							picPath: "path"+data[i]['picPath1'],
-							url: showItems.url+data[i]['itemId'],
+							picPath: "http://"+data[i]['picPath1'],
+							/*
+								参数在这里：
+								var itemId = data[i]['itemId'];  
+								var type = data[i]['type'];
+							*/
+							url: showItems.url+data[i]['itemId'],  
 							title: data[i]['name'],
 							price: data[i]['presentPrice']
 						});
